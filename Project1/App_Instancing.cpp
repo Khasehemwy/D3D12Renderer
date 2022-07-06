fcd5e8a4d204ee8bfed1abe4b69cca1878732617
Khasehemwy <BuluyuAzir@foxmail.com>
2022-07-06 15:39:18 +0800

    [Fix] app_instancing

--- Project1/App_Instancing.cpp
+++ Project1/App_Instancing.cpp
@@ -34,7 +34,7 @@ public:
 	virtual void OnMouseMove(WPARAM btnState, int x, int y)override;
 
 private:
-	const int mMaxInstanceNum = 8 * 10 * 10 * 10;
+	const int mMaxInstanceNum = 8 * 200 * 20 * 20;
 
 	ComPtr<ID3D12DescriptorHeap> mCbvHeap = nullptr;
 	void BuildDescriptorHeaps();
@@ -180,34 +180,7 @@ void InstancingApp::Draw(const GameTimer& gt)
 
 void InstancingApp::OnMouseMove(WPARAM btnState, int x, int y)
 {
-	if ((btnState & MK_LBUTTON) != 0)
-	{
-		// Make each pixel correspond to a quarter of a degree.
-		float dx = XMConvertToRadians(0.25f * static_cast<float>(x - mLastMousePos.x));
-		float dy = XMConvertToRadians(0.25f * static_cast<float>(y - mLastMousePos.y));
-
-		// Update angles based on input to orbit camera around box.
-		mTheta -= dx;
-		mPhi -= dy;
-
-		// Restrict the angle mPhi.
-		mPhi = MathHelper::Clamp(mPhi, 0.1f, MathHelper::Pi - 0.1f);
-	}
-	else if ((btnState & MK_RBUTTON) != 0)
-	{
-		// Make each pixel correspond to 0.005 unit in the scene.
-		float dx = 1.05f * static_cast<float>(x - mLastMousePos.x);
-		float dy = 1.05f * static_cast<float>(y - mLastMousePos.y);
-
-		// Update the camera radius based on input.
-		mRadius += dx - dy;
-
-		// Restrict the radius.
-		//mRadius = MathHelper::Clamp(mRadius, 3.0f, 15.0f);
-	}
-
-	mLastMousePos.x = x;
-	mLastMousePos.y = y;
+	MyApp::OnMouseMove(btnState, x, y);
 }
 
 void InstancingApp::BuildDescriptorHeaps()
