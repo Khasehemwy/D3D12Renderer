fb29e85bafb3995f6ff5fea831e17d7a3f16efaa
Khasehemwy <BuluyuAzir@foxmail.com>
2022-07-18 18:07:40 +0800

    [Update] RenderTexture (temp)

--- Project1/RenderTexture.cpp
+++ Project1/RenderTexture.cpp
@@ -5,7 +5,7 @@ RenderTexture::RenderTexture(ID3D12Device* device, UINT width, UINT height, DXGI
 	md3dDevice = device;
 	mWidth = width;
 	mHeight = height;
-	mFormat = format;
+	mSrvFormat = format;
 
 	BuildResources();
 }
@@ -26,6 +26,32 @@ void RenderTexture::OnResize(UINT newWidth, UINT newHeight)
 	}
 }
 
+void RenderTexture::BuildDescriptors(
+	CD3DX12_CPU_DESCRIPTOR_HANDLE hCpuSrv, 
+	CD3DX12_GPU_DESCRIPTOR_HANDLE hGpuSrv, 
+	CD3DX12_CPU_DESCRIPTOR_HANDLE hCpuDsv,
+	DXGI_FORMAT srvFormat)
+{
+	// Save references to the descriptors. 
+	mhCpuSrv = hCpuSrv;
+	mhGpuSrv = hGpuSrv;
+	mhCpuDsv = hCpuDsv;
+	mSrvFormat = srvFormat;
+
+	//  Create the descriptors
+	BuildDescriptors();
+}
+
+CD3DX12_CPU_DESCRIPTOR_HANDLE RenderTexture::Srv() const
+{
+	return mhCpuSrv;
+}
+
+CD3DX12_CPU_DESCRIPTOR_HANDLE RenderTexture::Dsv() const
+{
+	return mhCpuDsv;
+}
+
 void RenderTexture::BuildResources()
 {
 	// Note, compressed formats cannot be used for UAV.  We get error like:
@@ -42,7 +68,7 @@ void RenderTexture::BuildResources()
 	texDesc.Height = mHeight;
 	texDesc.DepthOrArraySize = 1;
 	texDesc.MipLevels = 1;
-	texDesc.Format = mFormat;
+	texDesc.Format = mSrvFormat;
 	texDesc.SampleDesc.Count = 1;
 	texDesc.SampleDesc.Quality = 0;
 	texDesc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
@@ -56,4 +82,26 @@ void RenderTexture::BuildResources()
 		nullptr,
 		IID_PPV_ARGS(&mRenderTex)
 	);
+}
+
+void RenderTexture::BuildDescriptors()
+{
+	// Create SRV to resource so we can sample the shadow map in a shader program.
+	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
+	srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+	srvDesc.Format = mSrvFormat;
+	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+	srvDesc.Texture2D.MostDetailedMip = 0;
+	srvDesc.Texture2D.MipLevels = 1;
+	srvDesc.Texture2D.ResourceMinLODClamp = 0.0f;
+	srvDesc.Texture2D.PlaneSlice = 0;
+	md3dDevice->CreateShaderResourceView(mRenderTex.Get(), &srvDesc, mhCpuSrv);
+
+	// Create DSV to resource so we can render to the shadow map.
+	D3D12_DEPTH_STENCIL_VIEW_DESC dsvDesc = {};
+	dsvDesc.Flags = D3D12_DSV_FLAG_NONE;
+	dsvDesc.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;
+	dsvDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	dsvDesc.Texture2D.MipSlice = 0;
+	//md3dDevice->CreateDepthStencilView(mRenderTex.Get(), &dsvDesc, mhCpuDsv);
 }
--- Project1/RenderTexture.h
+++ Project1/RenderTexture.h
@@ -13,14 +13,28 @@ public:
 
 	void OnResize(UINT newWidth, UINT newHeight);
 
+	void BuildDescriptors(
+		CD3DX12_CPU_DESCRIPTOR_HANDLE hCpuSrv,
+		CD3DX12_GPU_DESCRIPTOR_HANDLE hGpuSrv,
+		CD3DX12_CPU_DESCRIPTOR_HANDLE hCpuDsv,
+		DXGI_FORMAT srvFormat = DXGI_FORMAT_R8G8B8A8_UNORM);
+
+	CD3DX12_CPU_DESCRIPTOR_HANDLE Srv()const;
+	CD3DX12_CPU_DESCRIPTOR_HANDLE Dsv()const;
+
 private:
 	void BuildResources();
+	void BuildDescriptors();
 
 	ID3D12Device* md3dDevice = nullptr;
 
 	UINT mWidth = 0;
 	UINT mHeight = 0;
-	DXGI_FORMAT mFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
+	DXGI_FORMAT mSrvFormat = DXGI_FORMAT_R8G8B8A8_UNORM;
+
+	CD3DX12_CPU_DESCRIPTOR_HANDLE mhCpuSrv;
+	CD3DX12_GPU_DESCRIPTOR_HANDLE mhGpuSrv;
+	CD3DX12_CPU_DESCRIPTOR_HANDLE mhCpuDsv;
 
 	Microsoft::WRL::ComPtr<ID3D12Resource> mRenderTex = nullptr;
 };
