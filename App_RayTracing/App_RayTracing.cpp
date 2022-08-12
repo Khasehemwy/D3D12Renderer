f49a5a79d4750116dfed4e2b1d35d9861698b2ea
Khasehemwy <BuluyuAzir@foxmail.com>
2022-08-12 20:19:02 +0800

    [Update] App_RayTracing (Incomplete)

---
+++ App_RayTracing/App_RayTracing.cpp
@@ -0,0 +1,548 @@
+#include "Common/MathHelper.h"
+#include "Common/UploadBuffer.h"
+#include "Common/GeometryGenerator.h"
+#include "MyApp.h"
+#include "Model.h"
+using Microsoft::WRL::ComPtr;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+const int gNumFrameResources = 3;
+
+struct Vertex
+{
+	XMFLOAT3 pos;
+	XMFLOAT4 color;
+};
+
+struct RenderItem
+{
+	RenderItem() = default;
+
+	XMFLOAT4X4 World = MathHelper::Identity4x4();
+
+	int NumFramesDirty = gNumFrameResources;
+
+	UINT ObjCBIndex = -1;
+
+	MeshGeometry* Geo = nullptr;
+
+	D3D12_PRIMITIVE_TOPOLOGY PrimitiveType = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+
+	UINT IndexCount = 0;
+	UINT StartIndexLocation = 0;
+	int BaseVertexLocation = 0;
+};
+
+struct ObjectConstants
+{
+	DirectX::XMFLOAT4X4 World = MathHelper::Identity4x4();
+};
+
+struct PassConstants
+{
+	DirectX::XMFLOAT4X4 View = MathHelper::Identity4x4();
+	DirectX::XMFLOAT4X4 Proj = MathHelper::Identity4x4();
+};
+
+struct FrameResource
+{
+public:
+	FrameResource(ID3D12Device* device, UINT passCount, UINT objectCount)
+	{
+		device->CreateCommandAllocator(
+			D3D12_COMMAND_LIST_TYPE_DIRECT,
+			IID_PPV_ARGS(CmdListAlloc.GetAddressOf())
+		);
+
+		PassCB = std::make_unique<UploadBuffer<PassConstants>>(device, passCount, true);
+		ObjectCB = std::make_unique<UploadBuffer<ObjectConstants>>(device, objectCount, true);
+	};
+
+	Microsoft::WRL::ComPtr<ID3D12CommandAllocator> CmdListAlloc;
+
+	std::unique_ptr<UploadBuffer<PassConstants>> PassCB = nullptr;
+	std::unique_ptr<UploadBuffer<ObjectConstants>> ObjectCB = nullptr;
+
+	UINT64 Fence = 0;
+};
+
+class RayTracing :public MyApp
+{
+public:
+	RayTracing(HINSTANCE hInstance);
+
+	virtual bool Initialize()override;
+	virtual void Update(const GameTimer& gt)override;
+	virtual void Draw(const GameTimer& gt)override;
+private:
+	ComPtr<ID3D12Device5> mDxrDevice;
+	ComPtr<ID3D12GraphicsCommandList5> mDxrCommandList;
+	ComPtr<ID3D12StateObject> mDxrStateObject;
+
+	enum class RootSigParams : int {
+		OutputViewSlot = 0,
+		AccelerationStructureSlot,
+		SceneConstantSlot,
+		ObjConstantSlot,
+		VertexBuffersSlot,
+		Size
+	};
+	ComPtr<ID3D12RootSignature> mRootSignature = nullptr;
+	void BuildRootSignature();
+
+	std::unordered_map<std::string, ComPtr<ID3DBlob>> mShaders;
+	std::vector<D3D12_INPUT_ELEMENT_DESC>mInputLayout;
+	void BuildShadersAndInputLayout();
+
+	std::unordered_map<std::string, std::unique_ptr<Model>> mModels;
+	void LoadModels();
+
+	std::vector<std::unique_ptr<RenderItem>> mAllRenderitems;
+	std::vector<RenderItem*> mOpaqueRenderitems;
+	void BuildRenderItems();
+
+	std::vector<std::unique_ptr<FrameResource>> mFrameResources;
+	int mCurrFrameResourceIndex = 0;
+	FrameResource* mCurrFrameResource = nullptr;
+	void BuildFrameResources();
+
+	UINT mPassCbvOffset = 0;
+	ComPtr<ID3D12DescriptorHeap> mCbvHeap = nullptr;
+	PassConstants mMainPassCB;
+	void BuildDescriptorHeaps();
+	void BuildBuffers();
+
+	std::unordered_map<std::string, ComPtr<ID3D12PipelineState>> mPSOs;
+	void BuildPSOs();
+
+	void DrawRenderItems(ID3D12GraphicsCommandList* cmdList, const std::vector<RenderItem*>& ritems);
+};
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
+	PSTR cmdLine, int showCmd)
+{
+	// Enable run-time memory check for debug builds.
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	try
+	{
+		RayTracing theApp(hInstance);
+		if (!theApp.Initialize())
+			return 0;
+
+		return theApp.Run();
+	}
+	catch (DxException& e)
+	{
+		MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
+		return 0;
+	}
+}
+
+RayTracing::RayTracing(HINSTANCE hInstance) :
+	MyApp(hInstance)
+{
+}
+
+bool RayTracing::Initialize()
+{
+	if (!MyApp::Initialize())return false;
+
+	ThrowIfFailed(md3dDevice->QueryInterface(IID_PPV_ARGS(&mDxrDevice)), L"Couldn't get DirectX Raytracing interface for the device.\n");
+	ThrowIfFailed(mCommandList->QueryInterface(IID_PPV_ARGS(&mDxrCommandList)), L"Couldn't get DirectX Raytracing interface for the command list.\n");
+
+	mCamera.SetPosition(XMFLOAT3(0, 5, -50));
+
+	mCommandList->Reset(mDirectCmdListAlloc.Get(), nullptr);
+
+	BuildRootSignature();
+	BuildShadersAndInputLayout();
+	LoadModels();
+	BuildRenderItems();
+	BuildFrameResources();
+
+	BuildDescriptorHeaps();
+	BuildBuffers();
+
+	BuildPSOs();
+
+	mCommandList->Close();
+	ID3D12CommandList* cmdsLists[] = { mCommandList.Get() };
+	mCommandQueue->ExecuteCommandLists(_countof(cmdsLists), cmdsLists);
+
+	FlushCommandQueue();
+
+	return true;
+}
+
+void RayTracing::Update(const GameTimer& gt)
+{
+	MyApp::Update(gt);
+
+	mCurrFrameResourceIndex = (mCurrFrameResourceIndex + 1) % gNumFrameResources;
+	mCurrFrameResource = mFrameResources[mCurrFrameResourceIndex].get();
+
+	//todo:not understand
+	if (mCurrFrameResource->Fence != 0 && mFence->GetCompletedValue() < mCurrFrameResource->Fence)
+	{
+		HANDLE eventHandle = CreateEventEx(nullptr, false, false, EVENT_ALL_ACCESS);
+		ThrowIfFailed(mFence->SetEventOnCompletion(mCurrFrameResource->Fence, eventHandle));
+		WaitForSingleObject(eventHandle, INFINITE);
+		CloseHandle(eventHandle);
+	}
+	//:todo
+
+	//Update Per Object CB
+	auto currObjectCB = mCurrFrameResource->ObjectCB.get();
+	for (auto& e : mAllRenderitems)
+	{
+		// Only update the cbuffer data if the constants have changed.  
+		// This needs to be tracked per frame resource.
+		if (e->NumFramesDirty > 0)
+		{
+			XMMATRIX world = XMLoadFloat4x4(&e->World);
+
+			ObjectConstants objConstants;
+			XMStoreFloat4x4(&objConstants.World, XMMatrixTranspose(world));
+
+			currObjectCB->CopyData(e->ObjCBIndex, objConstants);
+
+			// Next FrameResource need to be updated too.
+			e->NumFramesDirty--;
+		}
+	}
+
+	//Update Main Pass Constant Buffer
+	XMMATRIX view = XMLoadFloat4x4(&mView);
+	XMMATRIX proj = XMLoadFloat4x4(&mProj);
+
+	XMStoreFloat4x4(&mMainPassCB.View, XMMatrixTranspose(view));
+	XMStoreFloat4x4(&mMainPassCB.Proj, XMMatrixTranspose(proj));
+	mMainPassCB.EyePosWorld = mEyePos;
+
+	auto currPassCB = mCurrFrameResource->PassCB.get();
+	currPassCB->CopyData(0, mMainPassCB);
+}
+
+void RayTracing::Draw(const GameTimer& gt)
+{
+	auto cmdListAlloc = mCurrFrameResource->CmdListAlloc;
+	cmdListAlloc->Reset();
+
+	if (mIsWireframe) {
+		mCommandList->Reset(cmdListAlloc.Get(), mPSOs["opaque_wireframe"].Get());
+	}
+	else {
+		mCommandList->Reset(cmdListAlloc.Get(), mPSOs["opaque"].Get());
+	}
+
+	mCommandList->RSSetViewports(1, &mScreenViewport);
+	mCommandList->RSSetScissorRects(1, &mScissorRect);
+
+	mCommandList->ResourceBarrier(
+		1, &CD3DX12_RESOURCE_BARRIER::Transition(
+			CurrentBackBuffer(),
+			D3D12_RESOURCE_STATE_PRESENT,
+			D3D12_RESOURCE_STATE_RENDER_TARGET)
+	);
+
+	mCommandList->ClearRenderTargetView(CurrentBackBufferView(), Colors::LightSteelBlue, 0, nullptr);
+	mCommandList->ClearDepthStencilView(DepthStencilView(), D3D12_CLEAR_FLAG_DEPTH | D3D12_CLEAR_FLAG_STENCIL, 1.0f, 0, 0, nullptr);
+
+	mCommandList->OMSetRenderTargets(1, &CurrentBackBufferView(), true, &DepthStencilView());
+
+	ID3D12DescriptorHeap* descriptorHeaps[] = { mCbvHeap.Get() };
+	mCommandList->SetDescriptorHeaps(_countof(descriptorHeaps), descriptorHeaps);
+
+	mCommandList->SetGraphicsRootSignature(mRootSignature.Get());
+
+	int passCbvIndex = mPassCbvOffset + mCurrFrameResourceIndex;
+	auto passCbvHandle = CD3DX12_GPU_DESCRIPTOR_HANDLE(mCbvHeap->GetGPUDescriptorHandleForHeapStart());
+	passCbvHandle.Offset(passCbvIndex, mCbvSrvUavDescriptorSize);
+	mCommandList->SetGraphicsRootDescriptorTable(1, passCbvHandle);
+
+	DrawRenderItems(mCommandList.Get(), mOpaqueRenderitems);
+
+	mCommandList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(CurrentBackBuffer(),
+		D3D12_RESOURCE_STATE_RENDER_TARGET, D3D12_RESOURCE_STATE_PRESENT));
+
+	mCommandList->Close();
+
+	ID3D12CommandList* cmdsLists[] = { mCommandList.Get() };
+	mCommandQueue->ExecuteCommandLists(_countof(cmdsLists), cmdsLists);
+
+	ThrowIfFailed(mSwapChain->Present(0, 0));
+	mCurrBackBuffer = (mCurrBackBuffer + 1) % SwapChainBufferCount;
+
+	mCurrFrameResource->Fence = ++mCurrentFence;
+
+	mCommandQueue->Signal(mFence.Get(), mCurrentFence);
+}
+
+void RayTracing::BuildFrameResources()
+{
+	for (int i = 0; i < gNumFrameResources; i++) {
+		mFrameResources.push_back(
+			std::make_unique<FrameResource>(
+				md3dDevice.Get(),
+				1, mAllRenderitems.size()
+				)
+		);
+	};
+}
+
+void RayTracing::BuildRootSignature()
+{
+	CD3DX12_DESCRIPTOR_RANGE ranges[2];
+	ranges[0].Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 2, 1);
+	ranges[1].Init(D3D12_DESCRIPTOR_RANGE_TYPE_UAV, 1, 0);
+
+	CD3DX12_ROOT_PARAMETER slotRootParameter[(UINT)RootSigParams::Size];
+	slotRootParameter[(UINT)RootSigParams::VertexBuffersSlot].InitAsDescriptorTable(1, &ranges[0]);
+	slotRootParameter[(UINT)RootSigParams::OutputViewSlot].InitAsDescriptorTable(1, &ranges[1]);
+	slotRootParameter[(UINT)RootSigParams::AccelerationStructureSlot].InitAsShaderResourceView(0);
+	slotRootParameter[(UINT)RootSigParams::SceneConstantSlot].InitAsConstantBufferView(0);
+	slotRootParameter[(UINT)RootSigParams::ObjConstantSlot].InitAsConstantBufferView(1);
+
+	CD3DX12_ROOT_SIGNATURE_DESC rootSigDesc(
+		(UINT)RootSigParams::Size, slotRootParameter, 0, nullptr,
+		D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT
+	);
+
+	ComPtr<ID3DBlob> serializedRootSig = nullptr;
+	ComPtr<ID3DBlob> errorBlob = nullptr;
+	HRESULT hr = D3D12SerializeRootSignature(
+		&rootSigDesc, D3D_ROOT_SIGNATURE_VERSION_1,
+		serializedRootSig.GetAddressOf(),
+		errorBlob.GetAddressOf()
+	);
+
+	md3dDevice->CreateRootSignature(
+		0,
+		serializedRootSig->GetBufferPointer(),
+		serializedRootSig->GetBufferSize(),
+		IID_PPV_ARGS(mRootSignature.GetAddressOf())
+	);
+}
+
+void RayTracing::BuildShadersAndInputLayout()
+{
+	// Create 7 subobjects that combine into a RTPSO:
+	// Subobjects need to be associated with DXIL exports (i.e. shaders) either by way of default or explicit associations.
+	// Default association applies to every exported shader entrypoint that doesn't have any of the same type of subobject associated with it.
+	// This simple sample utilizes default shader association except for local root signature subobject
+	// which has an explicit association specified purely for demonstration purposes.
+	// 1 - DXIL library
+	// 1 - Triangle hit group
+	// 1 - Shader config
+	// 2 - Local root signature and association
+	// 1 - Global root signature
+	// 1 - Pipeline config
+	CD3DX12_STATE_OBJECT_DESC raytracingPipeline{ D3D12_STATE_OBJECT_TYPE_RAYTRACING_PIPELINE };
+
+
+	// DXIL library
+	// This contains the shaders and their entrypoints for the state object.
+	// Since shaders are not considered a subobject, they need to be passed in via DXIL library subobjects.
+	auto lib = raytracingPipeline.CreateSubobject<CD3DX12_DXIL_LIBRARY_SUBOBJECT>();
+	D3D12_SHADER_BYTECODE libdxil = CD3DX12_SHADER_BYTECODE((void*)g_pRaytracing, ARRAYSIZE(g_pRaytracing));
+	lib->SetDXILLibrary(&libdxil);
+	// Define which shader exports to surface from the library.
+	// If no shader exports are defined for a DXIL library subobject, all shaders will be surfaced.
+	// In this sample, this could be ommited for convenience since the sample uses all shaders in the library. 
+	{
+		lib->DefineExport(c_raygenShaderName);
+		lib->DefineExport(c_closestHitShaderName);
+		lib->DefineExport(c_missShaderName);
+	}
+
+	// Triangle hit group
+	// A hit group specifies closest hit, any hit and intersection shaders to be executed when a ray intersects the geometry's triangle/AABB.
+	// In this sample, we only use triangle geometry with a closest hit shader, so others are not set.
+	auto hitGroup = raytracingPipeline.CreateSubobject<CD3DX12_HIT_GROUP_SUBOBJECT>();
+	hitGroup->SetClosestHitShaderImport(c_closestHitShaderName);
+	hitGroup->SetHitGroupExport(c_hitGroupName);
+	hitGroup->SetHitGroupType(D3D12_HIT_GROUP_TYPE_TRIANGLES);
+
+	// Shader config
+	// Defines the maximum sizes in bytes for the ray payload and attribute structure.
+	auto shaderConfig = raytracingPipeline.CreateSubobject<CD3DX12_RAYTRACING_SHADER_CONFIG_SUBOBJECT>();
+	UINT payloadSize = sizeof(XMFLOAT4);    // float4 pixelColor
+	UINT attributeSize = sizeof(XMFLOAT2);  // float2 barycentrics
+	shaderConfig->Config(payloadSize, attributeSize);
+
+	// Local root signature and shader association
+	// This is a root signature that enables a shader to have unique arguments that come from shader tables.
+	CreateLocalRootSignatureSubobjects(&raytracingPipeline);
+
+	// Global root signature
+	// This is a root signature that is shared across all raytracing shaders invoked during a DispatchRays() call.
+	auto globalRootSignature = raytracingPipeline.CreateSubobject<CD3DX12_GLOBAL_ROOT_SIGNATURE_SUBOBJECT>();
+	globalRootSignature->SetRootSignature(m_raytracingGlobalRootSignature.Get());
+
+	// Pipeline config
+	// Defines the maximum TraceRay() recursion depth.
+	auto pipelineConfig = raytracingPipeline.CreateSubobject<CD3DX12_RAYTRACING_PIPELINE_CONFIG_SUBOBJECT>();
+	// PERFOMANCE TIP: Set max recursion depth as low as needed 
+	// as drivers may apply optimization strategies for low recursion depths.
+	UINT maxRecursionDepth = 1; // ~ primary rays only. 
+	pipelineConfig->Config(maxRecursionDepth);
+
+#if _DEBUG
+	PrintStateObjectDesc(raytracingPipeline);
+#endif
+
+	// Create the state object.
+	ThrowIfFailed(m_dxrDevice->CreateStateObject(raytracingPipeline, IID_PPV_ARGS(&m_dxrStateObject)), L"Couldn't create DirectX Raytracing state object.\n");
+}
+
+void RayTracing::LoadModels()
+{
+	std::unique_ptr<Model> pacman = std::make_unique<Model>("pacman", "../resources/pacman/Pacman.stl", md3dDevice.Get(), mCommandList.Get());
+
+	mModels[pacman->Geo()->Name] = std::move(pacman);
+}
+
+void RayTracing::BuildRenderItems()
+{
+	int objCBIndex = 0;
+	for (auto& drawArg : mModels["pacman"]->Geo()->DrawArgs) {
+		auto renderItem = std::make_unique<RenderItem>();
+		XMStoreFloat4x4(&renderItem->World, XMMatrixRotationRollPitchYaw(XMConvertToRadians(90), 0.0f, 0.0f));
+		renderItem->ObjCBIndex = objCBIndex++;
+		renderItem->Geo = const_cast<MeshGeometry*>(mModels["pacman"]->Geo());
+		renderItem->PrimitiveType = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+		renderItem->IndexCount = drawArg.second.IndexCount;
+		renderItem->StartIndexLocation = drawArg.second.StartIndexLocation;
+		renderItem->BaseVertexLocation = drawArg.second.BaseVertexLocation;
+		mAllRenderitems.push_back(std::move(renderItem));
+	}
+
+	// All the render items are opaque.
+	for (auto& e : mAllRenderitems) mOpaqueRenderitems.push_back(e.get());
+}
+
+void RayTracing::BuildDescriptorHeaps()
+{
+	UINT objCount = (UINT)mOpaqueRenderitems.size();
+	UINT frameCount = gNumFrameResources;
+
+	UINT numDescriptors = objCount * gNumFrameResources + frameCount;
+
+	mPassCbvOffset = objCount * gNumFrameResources;
+
+	D3D12_DESCRIPTOR_HEAP_DESC cbvHeapDesc;
+	cbvHeapDesc.NumDescriptors = numDescriptors;
+	cbvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+	cbvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+	cbvHeapDesc.NodeMask = 0;
+	md3dDevice->CreateDescriptorHeap(&cbvHeapDesc, IID_PPV_ARGS(&mCbvHeap));
+}
+
+void RayTracing::BuildBuffers()
+{
+	UINT objCBByteSize = d3dUtil::CalcConstantBufferByteSize(sizeof(ObjectConstants));
+	UINT objCount = mOpaqueRenderitems.size();
+
+	for (int frameIndex = 0; frameIndex < gNumFrameResources; frameIndex++) {
+		auto objectCB = mFrameResources[frameIndex]->ObjectCB->Resource();
+
+		for (int objectIndex = 0; objectIndex < objCount; objectIndex++) {
+			D3D12_GPU_VIRTUAL_ADDRESS cbAddress = objectCB->GetGPUVirtualAddress();
+			cbAddress += objectIndex * objCBByteSize;
+
+			int heapIndex = objCount * frameIndex + objectIndex;
+			auto handle = CD3DX12_CPU_DESCRIPTOR_HANDLE(mCbvHeap->GetCPUDescriptorHandleForHeapStart());
+			handle.Offset(heapIndex, mCbvSrvUavDescriptorSize);
+
+			D3D12_CONSTANT_BUFFER_VIEW_DESC cbvDesc;
+			cbvDesc.BufferLocation = cbAddress;
+			cbvDesc.SizeInBytes = objCBByteSize;
+			md3dDevice->CreateConstantBufferView(&cbvDesc, handle);
+		}
+	}
+
+	UINT passCBByteSize = d3dUtil::CalcConstantBufferByteSize(sizeof(PassConstants));
+
+	for (int frameIndex = 0; frameIndex < gNumFrameResources; frameIndex++) {
+		auto passCB = mFrameResources[frameIndex]->PassCB->Resource();
+
+		D3D12_GPU_VIRTUAL_ADDRESS cbAddress = passCB->GetGPUVirtualAddress();
+		//cbAddress += frameIndex; 
+		//todo: why not add address?
+
+		int heapIndex = mPassCbvOffset + frameIndex;
+		auto handle = CD3DX12_CPU_DESCRIPTOR_HANDLE(mCbvHeap->GetCPUDescriptorHandleForHeapStart());
+		handle.Offset(heapIndex, mCbvSrvUavDescriptorSize);
+
+		D3D12_CONSTANT_BUFFER_VIEW_DESC cbvDesc;
+		cbvDesc.BufferLocation = cbAddress;
+		cbvDesc.SizeInBytes = passCBByteSize;
+		md3dDevice->CreateConstantBufferView(&cbvDesc, handle);
+	}
+}
+
+void RayTracing::BuildPSOs()
+{
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC opaquePsoDesc;
+
+	ZeroMemory(&opaquePsoDesc, sizeof(D3D12_GRAPHICS_PIPELINE_STATE_DESC));
+	opaquePsoDesc.InputLayout = { mInputLayout.data(), (UINT)mInputLayout.size() };
+	opaquePsoDesc.pRootSignature = mRootSignature.Get();
+	opaquePsoDesc.VS =
+	{
+		reinterpret_cast<BYTE*>(mShaders["standardVS"]->GetBufferPointer()),
+		mShaders["standardVS"]->GetBufferSize()
+	};
+	opaquePsoDesc.PS =
+	{
+		reinterpret_cast<BYTE*>(mShaders["opaquePS"]->GetBufferPointer()),
+		mShaders["opaquePS"]->GetBufferSize()
+	};
+
+	opaquePsoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
+	opaquePsoDesc.RasterizerState.FillMode = D3D12_FILL_MODE_SOLID;
+	opaquePsoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
+	opaquePsoDesc.DepthStencilState = CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT);
+	opaquePsoDesc.SampleMask = UINT_MAX;
+	opaquePsoDesc.PrimitiveTopologyType = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+	opaquePsoDesc.NumRenderTargets = 1;
+	opaquePsoDesc.RTVFormats[0] = mBackBufferFormat;
+	opaquePsoDesc.SampleDesc.Count = m4xMsaaState ? 4 : 1;
+	opaquePsoDesc.SampleDesc.Quality = m4xMsaaState ? (m4xMsaaQuality - 1) : 0;
+	opaquePsoDesc.DSVFormat = mDepthStencilFormat;
+	md3dDevice->CreateGraphicsPipelineState(&opaquePsoDesc, IID_PPV_ARGS(&mPSOs["opaque"]));
+
+	D3D12_GRAPHICS_PIPELINE_STATE_DESC opaqueWireframePsoDesc = opaquePsoDesc;
+	opaqueWireframePsoDesc.RasterizerState.FillMode = D3D12_FILL_MODE_WIREFRAME;
+	md3dDevice->CreateGraphicsPipelineState(&opaqueWireframePsoDesc, IID_PPV_ARGS(&mPSOs["opaque_wireframe"]));
+}
+
+void RayTracing::DrawRenderItems(ID3D12GraphicsCommandList* cmdList, const std::vector<RenderItem*>& ritems)
+{
+	UINT objCBByteSize = d3dUtil::CalcConstantBufferByteSize(sizeof(ObjectConstants));
+
+	auto objectCB = mCurrFrameResource->ObjectCB->Resource();
+
+	// For each render item...
+	for (size_t i = 0; i < ritems.size(); ++i)
+	{
+		auto ri = ritems[i];
+
+		cmdList->IASetVertexBuffers(0, 1, &ri->Geo->VertexBufferView());
+		cmdList->IASetIndexBuffer(&ri->Geo->IndexBufferView());
+		cmdList->IASetPrimitiveTopology(ri->PrimitiveType);
+
+		// Offset to the CBV in the descriptor heap for this object and for this frame resource.
+		UINT cbvIndex = mCurrFrameResourceIndex * (UINT)mOpaqueRenderitems.size() + ri->ObjCBIndex;
+		auto cbvHandle = CD3DX12_GPU_DESCRIPTOR_HANDLE(mCbvHeap->GetGPUDescriptorHandleForHeapStart());
+		cbvHandle.Offset(cbvIndex, mCbvSrvUavDescriptorSize);
+
+		cmdList->SetGraphicsRootDescriptorTable(0, cbvHandle);
+
+		cmdList->DrawIndexedInstanced(ri->IndexCount, 1, ri->StartIndexLocation, ri->BaseVertexLocation, 0);
+	}
+}
